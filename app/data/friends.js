e7287f71bbcf1146960053977fc6da74be58af34
cupururum <v.romashchenko@gmail.com>
2018-02-17 13:54:58 -0800

    works with heroku

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,2 +1,35 @@
-var friendsArray = []
+var friendsArray = [
+    {
+        "name": "Red Gyarados",
+        "photo": "https://cdn.bulbagarden.net/upload/5/52/Lance_Red_Gyarados.png",
+        "scores": [
+        "3",
+        "4",
+        "2",
+        "4",
+        "3",
+        "1",
+        "3",
+        "2",
+        "1",
+        "1"
+        ]
+    },
+    {
+        "name": "Kyurem",
+        "photo": "https://cdn.bulbagarden.net/upload/b/b2/646Kyurem_PMDGTI.png",
+        "scores": [
+        "4",
+        "1",
+        "3",
+        "2",
+        "1",
+        "4",
+        "1",
+        "3",
+        "2",
+        "4"
+        ]
+    }
+]
 module.exports = friendsArray
