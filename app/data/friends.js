704b76bf701e346ea203dbc80fce63bbcd646a81
cupururum <v.romashchenko@gmail.com>
2018-02-17 13:16:39 -0800

    works with heroku

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,50 +1,34 @@
 var friendsArray = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Red Gyarados",
+        "photo": "https://cdn.bulbagarden.net/upload/5/52/Lance_Red_Gyarados.png",
         "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
+        "3",
+        "4",
+        "2",
+        "4",
+        "3",
+        "1",
+        "3",
+        "2",
+        "1",
+        "1"
         ]
     },
     {
-        "name": "pikachu",
-        "photo": "https://en.wikipedia.org/wiki/Pikachu#/media/File:Pok%C3%A9mon_Pikachu_art.png",
+        "name": "Kyurem",
+        "photo": "https://cdn.bulbagarden.net/upload/b/b2/646Kyurem_PMDGTI.png",
         "scores": [
-            "3",
-            "2",
-            "3",
-            "5",
-            "4",
-            "3",
-            "4",
-            "5",
-            "1",
-            "2"
-        ]
-    },
-    {
-        "name": "pikachu2",
-        "photo": "https://en.wikipedia.org/wiki/Pikachu#/media/File:Pok%C3%A9mon_Pikachu_art.png",
-        "scores": [
-            "2",
-            "3",
-            "4",
-            "3",
-            "1",
-            "4",
-            "1",
-            "5",
-            "2",
-            "1"
+        "4",
+        "1",
+        "3",
+        "2",
+        "1",
+        "4",
+        "1",
+        "3",
+        "2",
+        "4"
         ]
     }
 ]
